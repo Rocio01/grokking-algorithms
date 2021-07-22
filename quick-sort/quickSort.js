26ab81f565b8af950c9b89a73da153d46d9f2025
Zulma Rocio <zrmartinezg@gmail.com>
2021-07-22 17:18:08 -0500

    Add function to count elements in an array with recursion in js

--- quick-sort/quickSort.js
+++ quick-sort/quickSort.js
@@ -1,12 +1,21 @@
-function recursive_sum(arr){
+function recursionSum(arr){
   if (arr.length == 0){
     return 0
   } else {
-   return arr[0] + recursive_sum(arr.slice(1))
+   return arr[0] + recursionSum(arr.slice(1))
   }
 
 }
 
+function itemsListCount(arr){
+  if (arr.length == 0){
+    return 0
+  } else {
+    return 1 + itemsListCount(arr.slice(1))
+  }
+}
+
 const my_arr = [1,2,3,4,5,6,7];
 
-console.log(recursive_sum(my_arr));
+console.log(recursionSum(my_arr));
+console.log(itemsListCount(my_arr));
