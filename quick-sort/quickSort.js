dc0177fc7da18fff22371f66fb8d283abf32595b
Zulma Rocio <zrmartinezg@gmail.com>
2021-07-22 17:31:21 -0500

    Add function to find the largest number in an array with recursion in js

--- quick-sort/quickSort.js
+++ quick-sort/quickSort.js
@@ -15,7 +15,27 @@ function itemsListCount(arr){
   }
 }
 
-const my_arr = [1,2,3,4,5,6,7];
+function maxNumber(arr){
+  if (arr.length === 0){
+    return null
+  }
+
+  else if (arr.length === 1){
+    return arr[0];
+  }
+  else {
+    max = maxNumber(arr.slice(1));
+    if(arr[0] > max ){
+      return arr[0]
+    }else{
+      return max
+    }
+  }
+}
+
+const my_arr = [1,2,3,4,5,6,7, 30];
 
 console.log(recursionSum(my_arr));
 console.log(itemsListCount(my_arr));
+
+console.log(maxNumber(my_arr));
